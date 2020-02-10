ba38073e8ee4951b877698621bbc0b397a6ab827
Luiz Otavio <lotavio18@hotmail.com>
2020-02-10 19:39:18 -0300

    Initial Commit

---
+++ To-Do List SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To-Do List SwiftUI
+//
+//  Created by Luiz Otavio on 10/02/20.
+//  Copyright © 2020 Luiz Otavio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
